2a3120bf04aa5cdcf80d8aa86646b1dc9f20b472
Suryansh808 <suryanshsaxena808@gmail.com>
2025-01-06 18:13:27 +0530

    lalalla

--- Backend/server.js
+++ Backend/server.js
@@ -24,7 +24,7 @@ const corsOptions = {
   allowedHeaders:["Content-Type" , "Authorization"],
 };
 
-app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000;
 // const DB_URI = process.env.DB_URI;
