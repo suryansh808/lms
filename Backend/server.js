8fa2598743d029432730247890cd4de78491d894
Suryansh808 <suryanshsaxena808@gmail.com>
2025-01-06 18:41:40 +0530

    hhrhh

--- Backend/server.js
+++ Backend/server.js
@@ -22,6 +22,7 @@ const corsOptions = {
   origin: process.env.FRONTEND_URL,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders:["Content-Type" , "Authorization"],
+  credentials: true ,
 };
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
